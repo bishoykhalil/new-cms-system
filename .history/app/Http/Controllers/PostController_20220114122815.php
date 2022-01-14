5c01c853e23ba85f77e0f6bde90cd3d90e2a1848
Bishoy Khalil helmy <74961440+bishoykhalil@users.noreply.github.com>
2022-01-14 16:23:31 +0200

    Initial commit

---
+++ .history/app/Http/Controllers/PostController_20220114122815.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class PostController extends Controller
+{
+
+    public function index()
+    {
+        $posts = Post::all();
+        return view('admin.posts.index',['posts'=>$posts]);
+    }
+
+
+    public function show(Post $post ){
+
+
+        return view('blog-post',['post' =>$post]);
+    }
+
+    public function create( ){
+        return view('admin.posts.create');
+    }
+
+    public function store(){
+
+     $inputs =   request()->validate([
+            'title'=>'required',
+            'post_image'=>'file',
+            'body'=>'required'
+        ]);
+
+if (request('post_image')){
+    $inputs ['post_image'] = request('post_image')->store('images');
+}
+
+        // remove
+        auth()->user()->posts()->create($inputs);
+return back();
+
+        //dd(request()->all());
+    }
+
+    public function destroy(Post $post){
+
+        $post->delete();
+         return redirect()->back()->withErrors($validator)->withInput();
+    }
+}
---
+++ .history/resources/views/admin/posts/edit.blade_20220114144033.php
@@ -0,0 +1,37 @@
+<x-admin-master>
+
+    @section('content')
+
+    <h1>Edit</h1>
+        <form method="post" action="{{route('post.store')}}" enctype="multipart/form-data" >
+        @csrf
+            <div class="form-group">
+                <label for="title">Title</label>
+                <input type="text"
+                       name="title"
+                       class="form-control"
+                       id="title"
+                       aria-describedby=""
+                       value="{{$post->title}}">
+            </div>
+            <div class="form-group">
+                <label for="file">File</label>
+                <input type="file"
+                       name="title"
+                       class="form-control"
+                       id="title"
+                       aria-describedby=""
+                       placeholder="Enter Title">
+            </div>
+            <div class="form-group">
+                <textarea class="form-group" name="body" id="body" cols="40" rows="10">{{$post->body}} </textarea>
+            </div>
+
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+
+
+
+    @endsection
+
+</x-admin-master>
